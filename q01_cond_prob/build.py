fa96113ecfd2896df64c5b4a466637d45d3d6964
mritunjaysingh07 <mj.singh07@gmail.com>
2017-10-11 18:38:34 +0000

    Done

--- q01_cond_prob/build.py
+++ q01_cond_prob/build.py
@@ -2,8 +2,14 @@
 from __future__ import division
 
 import pandas as pd
+def cond_prob(df):
+    #df= pd.read_csv('data/house_pricing.csv')
+    df = df
 
-df = pd.read_csv('data/house_pricing.csv')
+    all_houses = df.shape[0]
+    houses_in_OldTown = df[df['Neighborhood'] == 'OldTown'].shape[0]
+    #print('Probability of picking a house in OldTown: {}'.format((houses_in_OldTown/all_houses) * ((houses_in_OldTown - 1)/(all_houses - 1)) * ((houses_in_OldTown - 2)/(all_houses - 2))))
+    conditional_prob= ((houses_in_OldTown/all_houses) * ((houses_in_OldTown - 1)/(all_houses - 1)) * ((houses_in_OldTown - 2)/(all_houses - 2)))
+    return conditional_prob
 
 # Enter Code Here
-
