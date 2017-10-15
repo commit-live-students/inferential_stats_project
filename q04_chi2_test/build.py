41c7b25b6effb526ba7691be75f12d4000b0fb44
satishkhanna <snykhanna@gmail.com>
2017-10-15 10:31:37 +0000

    Done

--- q04_chi2_test/build.py
+++ q04_chi2_test/build.py
@@ -11,6 +11,10 @@ def chi_square(df):
     saleP = pd.qcut(df['SalePrice'],3,['High', 'Medium', 'Low'])
     freq = pd.crosstab(landS,saleP)
     chi2,pval,dof,expected = stats.chi2_contingency(freq)
-    p_value = pval.item()
-    test_result = np.bool_(pval > 0.05)
-    return p_value,test_result
+
+    if pval < 0.05:
+        test_result = True
+    else:
+        test_result = False
+
+    return float(pval),np.bool_(test_result)
