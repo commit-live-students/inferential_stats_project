6fbe5804f7132ef4e995de9aa99fd347ffb1abf2
samir0719 <sameer_0719@yahoo.com>
2017-10-15 09:01:36 +0000

    Done

--- q04_chi2_test/build.py
+++ q04_chi2_test/build.py
@@ -1,8 +1,18 @@
+# %load q04_chi2_test/build.py
 # Default imports
 import scipy.stats as stats
 import pandas as pd
+import numpy as np
 
 df = pd.read_csv('data/house_pricing.csv')
-
+def chi_square(df):
+    pr=pd.qcut(df['SalePrice'],3,labels=['High','Medium','Low'])
 # Enter Code Here
-
+    freqtab=pd.crosstab(df['LandSlope'],pr)
+#print freqtab
+    chi2,pval,dof,expected = stats.chi2_contingency(freqtab)
+    if pval < 0.05:
+        test_res=True
+    else:
+        test_res=False
+    return float(pval),np.bool_(test_res)
