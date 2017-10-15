8729aa6c0264bc0d27a2b54498eef52e2d1b008f
sudharsanasai <Sudharsanasai@gmail.com>
2017-10-15 15:42:00 +0000

    Done

--- q03_t_test/build.py
+++ q03_t_test/build.py
@@ -5,4 +5,12 @@ import pandas as pd
 df = pd.read_csv('data/house_pricing.csv')
 
 # Enter Code Here
+def t_statistic(df):
+    pop = df['GrLivArea']
+    old_town = df[df['Neighborhood']=='OldTown']['GrLivArea']
 
+    pop_mean = pop.mean()
+    pop_std = pop.std()
+
+    res = stats.ttest_1samp(old_town,pop_mean)
+    return res[1], (res[1] < 0.05 or res[1] > 0.95)
