0e7b332a42365916ba4cdaec6d993e0fa3963f58
Mohammed-Sunasra <sunasramohd@gmail.com>
2017-10-17 18:05:15 +0000

    Done

--- q02_confidence_interval/build.py
+++ q02_confidence_interval/build.py
@@ -8,3 +8,12 @@ sample = df['GrLivArea']
 
 
 # Write your solution here :
+def confidence_interval(sample):
+    z_critical = 1.645#for 90% Confidence interval
+    std_dev_sample = sample.std()
+
+    margin_of_error = z_critical * (std_dev_sample / math.sqrt(len(sample)))
+    interval = ((sample.mean() - margin_of_error), (sample.mean() + margin_of_error))
+    return interval
+
+confidence_interval(sample)
