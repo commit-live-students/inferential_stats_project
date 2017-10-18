dc20414b6eb9027cf1cbc0aff292d8d624ffaaf2
melviii100 <melvinjose1992@gmail.com>
2017-10-18 07:40:42 +0000

    Done

--- q04_chi2_test/build.py
+++ q04_chi2_test/build.py
@@ -5,4 +5,10 @@ import pandas as pd
 df = pd.read_csv('data/house_pricing.csv')
 
 # Enter Code Here
+def chi_square(df):
+    q = pd.qcut(df.SalePrice , q=3)
+    print q
+    #return p_value, test_result
 
+
+print chi_square(df)
