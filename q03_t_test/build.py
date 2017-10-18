c58e8fac310254571f62ff063d72385b34d0c44b
ashwinij9 <ashwinijoshi9793@gmail.com>
2017-10-18 16:22:11 +0000

    Done

--- q03_t_test/build.py
+++ q03_t_test/build.py
@@ -1,8 +1,14 @@
 # Default imports
 import scipy.stats as stats
 import pandas as pd
+from statsmodels.stats.weightstats import ztest
 
 df = pd.read_csv('data/house_pricing.csv')
+def t_statistic(df):
+    a,b = stats.ttest_1samp(a= df[df['Neighborhood'] == 'OldTown']['GrLivArea'],popmean= df['GrLivArea'].mean())
+    return b,b<0.025
+
+
+
 
 # Enter Code Here
-
