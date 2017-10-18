60f08a5bb651255fb2ff493ecaf26fe1026070e8
swapnilrt <swapnil13488@gmail.com>
2017-10-18 18:26:18 +0000

    Done

--- q03_t_test/build.py
+++ q03_t_test/build.py
@@ -1,8 +1,12 @@
+# %load q03_t_test/build.py
 # Default imports
 import scipy.stats as stats
 import pandas as pd
 
 df = pd.read_csv('data/house_pricing.csv')
 
-# Enter Code Here
-
+def t_statistic(df):
+    x = df[df['Neighborhood'] == 'OldTown']['GrLivArea']
+    stat, pvalue = stats.ttest_1samp(a=x, popmean=df['GrLivArea'].mean())
+    endpt = stats.t.ppf(q=0.050,df=112)
+    return pvalue, pvalue<endpt
