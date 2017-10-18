b73c2bf9dbd1ac65ee52d2e5fc97aef4f97e1cee
saylijawale123 <saylijawale123@gmail.com>
2017-10-18 03:22:52 +0000

    Done

--- q02_confidence_interval/build.py
+++ q02_confidence_interval/build.py
@@ -5,6 +5,15 @@ import pandas as pd
 
 df = pd.read_csv('data/house_pricing.csv')
 sample = df['GrLivArea']
-
+def confidence_interval(sample):
+    sample_size = 1460
+    sample_mean = sample.mean()
+    z_critical = 1.64485362695
+    sigma = sample.std()
+    standard_error = sigma/math.sqrt(sample_size)
+    Estimate = (z_critical)*(standard_error)
+    lower_limit = sample_mean - Estimate
+    upper_limit = sample_mean + Estimate
+    return lower_limit,upper_limit
 
 # Write your solution here :
