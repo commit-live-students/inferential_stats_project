53ccea21717d7945fc99c384038e8f3266c5d0f9
swapnilrt <swapnil13488@gmail.com>
2017-10-18 18:23:47 +0000

    Done

--- q02_confidence_interval/build.py
+++ q02_confidence_interval/build.py
@@ -8,3 +8,11 @@ sample = df['GrLivArea']
 
 
 # Write your solution here :
+def confidence_interval(sample):
+    n = sample.shape[0]
+    sigma = sample.mean()
+    # z = 1.645
+    z = stats.norm.ppf(q = 0.95)
+    std_dev = sample.std()
+    std_error = z*(std_dev/math.sqrt(n))
+    return sigma-std_error,sigma+std_error
