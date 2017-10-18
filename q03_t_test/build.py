252c3ee98f7d122513a87954279a0d0026b69be7
gaurav-barclays <gaurav.simply@gmail.com>
2017-10-18 17:55:33 +0000

    Done

--- q03_t_test/build.py
+++ q03_t_test/build.py
@@ -1,8 +1,18 @@
+# %load q03_t_test/build.py
 # Default imports
 import scipy.stats as stats
 import pandas as pd
+import numpy as np
 
 df = pd.read_csv('data/house_pricing.csv')
+def t_statistic(df):
+    res=stats.ttest_1samp(a= df[df['Neighborhood'] == 'OldTown']['GrLivArea'],               # Sample data
+                 popmean= df['GrLivArea'].mean())  # Pop mean
+    p_value = res[1]
+    if p_value == 0.9:
+        test_res=True
+    else:
+        test_res=False
+    return p_value,np.bool_(test_res)
 
 # Enter Code Here
-
