3221d2e4f34347f5b1d7ee92418b9379fa69dcf5
vinod-boga <vinodboga17@gmail.com>
2017-10-26 18:18:34 +0000

    Done

--- q01_cond_prob/build.py
+++ q01_cond_prob/build.py
@@ -2,8 +2,13 @@
 from __future__ import division
 
 import pandas as pd
+import numpy as np
 
 df = pd.read_csv('data/house_pricing.csv')
 
-# Enter Code Here
+def cond_prob(df):
+    a = df[df['Neighborhood']=='OldTown']['Neighborhood'].count()
+    b = df.shape[0]
+    conditional_prob = (a/b)*((a-1)/(b-1))*((a-2)/(b-2))
 
+    return conditional_prob
