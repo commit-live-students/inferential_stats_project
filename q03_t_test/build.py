55f7b1f6e9ede343f1262cb9f2e4a1ba26811a75
shashi07 <shashi.mishra43@gmail.com>
2017-10-30 07:41:30 +0000

    Done

--- q03_t_test/build.py
+++ q03_t_test/build.py
@@ -4,5 +4,12 @@ import pandas as pd
 
 df = pd.read_csv('data/house_pricing.csv')
 
-# Enter Code Here
+def t_statistic(df):
+    alpha=0.05
+    fil1=df['Neighborhood']=='OldTown'
+    newdf=df.loc[fil1,'GrLivArea']
+    s,p_value=stats.ttest_1samp(newdf,df.loc[:,'GrLivArea'].mean())
+    result=p_value<alpha
+    return p_value,result
 
+# Enter Code Here
