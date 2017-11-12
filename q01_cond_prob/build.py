d9ac977c06d4e2b722ef884e4a8ebd93000af6ff
sandiprb <baradiya.sandip@gmail.com>
2017-11-12 16:44:15 +0000

    Done

--- q01_cond_prob/build.py
+++ q01_cond_prob/build.py
@@ -6,4 +6,12 @@ import pandas as pd
 df = pd.read_csv('data/house_pricing.csv')
 
 # Enter Code Here
-
+def cond_prob(df):
+    for_outcomes = df[df['Neighborhood'] == 'OldTown'].shape[0]
+    sample_space = df.shape[0]
+    steps = 3
+    p = 1
+    for i in range(0, steps):
+        p = p * ((for_outcomes - i)/(sample_space - i))
+        
+    return p
