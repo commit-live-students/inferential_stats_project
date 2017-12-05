2171088433bfc2c5fd2b5db1a97bfc601b2bd128
AshlinKarkada <ashlin@greyatom.com>
2017-12-05 08:19:40 +0000

    Done

--- q02_confidence_interval/build.py
+++ q02_confidence_interval/build.py
@@ -1,15 +1,18 @@
-# Default imports
 import math
+import numpy as np
 import scipy.stats as stats
-import pandas as pd
+
+from greyatomlib import pandas_project as pd
 
 df = pd.read_csv('data/house_pricing.csv')
-sample = df['GrLivArea']
 
+np.random.seed(10)
+sample = np.random.choice(a=df['GrLivArea'], size=500)
 
-# Write your solution here :
-
-
-
-
-
+def confidence_interval(sample=sample):
+    sample_mean = sample.mean()
+    z_critical = stats.norm.ppf(q=0.95)
+    stdev = sample.std()
+    standard_error = z_critical * (stdev / math.sqrt(sample.shape[0]))
+    confidence_interval = (sample_mean - standard_error, sample_mean + standard_error)
+    return confidence_interval
--- q03_t_test/build.py
+++ q03_t_test/build.py
@@ -1,9 +1,8 @@
-# Default imports
 import scipy.stats as stats
 import pandas as pd
 
 df = pd.read_csv('data/house_pricing.csv')
 
-
-# Enter Code Here
-
+def t_statistic(df):
+    t_statistic, p_value = stats.ttest_1samp(a=df[df['Neighborhood']=='OldTown']['GrLivArea'], popmean=df['GrLivArea'].mean())
+    return p_value, p_value < 0.05
