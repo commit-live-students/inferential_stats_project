d9daa63212f34e4ca58f7e3bc0f856ef77aa45e7
Bijit Deka <bijitdeka23@gmail.com>
2017-12-13 17:08:26 +0530

    modified 12 and q3

--- q02_confidence_interval/tests/test_q02_confidence_interval.py
+++ q02_confidence_interval/tests/test_q02_confidence_interval.py
@@ -13,17 +13,23 @@ class TestConfidence_interval(TestCase):
         args = getargspec(confidence_interval)
         self.assertEqual(len(args[0]), 1, "Expected arguments %d, Given %d" % (1, len(args[0])))
 
-    def test_confidence_interval_result_type(self):  # Return type tests
+    def test_confidence_interval_result_low_type(self):  # Return type tests
         low, high = confidence_interval(sample)
         self.assertIsInstance(low, float, "Expected data type for `Confidence Interval` is float you are returning\
         %s" % (type(low)))
+
+    def test_confidence_interval_result_high_type(self):
+        low, high = confidence_interval(sample)
         self.assertIsInstance(high, float, "Expected data type for `Confidence Interval` is float you are returning\
         %s" % (type(high)))
 
-    def test_confidence_interval_result_values(self):  # Return value tests
+    def test_confidence_interval_result_low_values(self):  # Return value tests
         low, high = confidence_interval(sample)
         self.assertEqual(low, 1492.8429310773924,"Return `Confidence Interval` value does not \
         match expected value")
+
+    def test_confidence_interval_result_hugh_values(self):
+        low, high = confidence_interval(sample)
         self.assertEqual(high, 1538.0844661828817, "Return `Confidence Interval` value does not \
         match expected value")
 
--- q03_t_test/tests/test_q03_t_test.py
+++ q03_t_test/tests/test_q03_t_test.py
@@ -16,15 +16,21 @@ class TestTTest(TestCase):
         args = getargspec(t_statistic)
         self.assertEqual(args[3], None, "Expected default values do not match given default values")
 
-    def test_t_test_result_type(self):        # Return type tests
+    def test_t_test_result_low_type(self):        # Return type tests
         pval, result = t_statistic(df)
         self.assertIsInstance(pval, float, "Expected data type for `Hypothesis Testing` is float you are returning\
                 %s" % (type(pval)))
+
+    def test_t_test_result_high_type(self):
+        pval, result = t_statistic(df)
         self.assertIsInstance(result, numpy.bool_, "Expected data type for `Hypothesis Testing` is bool you are returning\
                 %s" % (type(result)))
 
-    def test_t_test_result_values(self):  # Return result values
+    def test_t_test_result_low_values(self):  # Return result values
         pval, result = t_statistic(df)
         self.assertAlmostEqual(pval, 0.51158698884870502, 3, "Return `Hypothesis Testing` value does not \
         match expected value")
+
+    def test_t_test_result_high_values(self):
+        pval, result = t_statistic(df)
         self.assertFalse(result, "Return `Hypothesis Testing` value does not match expected value")
