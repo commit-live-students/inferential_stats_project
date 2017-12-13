fa4fd5f614fb24defc7c065f5d71585d8be43f38
chiragpujari <chg.p786@gmail.com>
2017-12-13 09:59:33 +0000

    Done

--- q02_confidence_interval/build.py
+++ q02_confidence_interval/build.py
@@ -8,5 +8,15 @@ sample = df['GrLivArea']
 
 
 # Write your solution here :
+def confidence_interval(sample):
+    sample_mean = sample.mean()
 
+    z_critical = stats.norm.ppf(q = 0.95)  # Get the z-critical value*
 
+    pop_stdev = df['GrLivArea'].std()  # Get the population standard deviation
+
+    margin_of_error = z_critical * (pop_stdev/math.sqrt(sample.shape[0]))
+
+    confidence_interval = (sample_mean - margin_of_error,
+                           sample_mean + margin_of_error)
+    return confidence_interval
