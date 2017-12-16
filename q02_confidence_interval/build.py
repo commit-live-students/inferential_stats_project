0ebcf27c7917a3e5d9c3c242ca113be78d7c8728
KRSatpute <ksatpute123@gmail.com>
2017-12-16 19:23:53 +0000

    Done

--- q02_confidence_interval/build.py
+++ q02_confidence_interval/build.py
@@ -8,8 +8,6 @@ sample = df['GrLivArea']
 
 
 # Write your solution here :
-
-
-
-
-
+def confidence_interval(sample):
+    size, _, mean, var, _, _ = stats.describe(sample)
+    return stats.norm.interval(0.9, loc=mean, scale=math.sqrt(var / size))
