4525e08760a3769b11e0dba350746a2b110faecf
vp999 <patilvish@gmail.com>
2017-12-17 11:02:47 +0000

    Done

--- q02_confidence_interval/build.py
+++ q02_confidence_interval/build.py
@@ -8,5 +8,16 @@ sample = df['GrLivArea']
 
 
 # Write your solution here :
+import scipy.stats as stats
 
+def confidence_interval(sample):
+    z_critical = stats.norm.ppf(q = 0.95)  # Get the z-critical valu
+    sample_mean = sample.mean()
+    sample_std = sample.std()
+    sample_size = sample.size
+    estimate_of_error = z_critical * (sample_std/math.sqrt(sample_size))
+    lower_limit = sample_mean - estimate_of_error
+    upper_limit = sample_mean + estimate_of_error
+    return(lower_limit,upper_limit)
 
+print confidence_interval(sample)
