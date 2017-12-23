8915aa776126f5cb48f7162d5b22f241b82b2232
NeelkanthMehta <neilstrong2003@yahoo.com>
2017-12-23 11:57:25 +0000

    Done

--- q01_cond_prob/build.py
+++ q01_cond_prob/build.py
@@ -2,11 +2,17 @@
 from __future__ import division
 
 import pandas as pd
-
+import numpy as np
 df = pd.read_csv('data/house_pricing.csv')
 
-
 # Enter Code Here
-
-
-
+def cond_prob(data):
+    all_houses = df['Neighborhood'].count()
+    house_oldTown = df[df['Neighborhood'] == 'OldTown'].shape[0]
+    proarr = []
+    for i in range(0,3):
+        prob = house_oldTown/ all_houses
+        proarr.append(prob)
+        all_houses -= 1
+        house_oldTown -= 1
+    return np.product(proarr)
--- q02_confidence_interval/build.py
+++ q02_confidence_interval/build.py
@@ -1,12 +1,15 @@
 # Default imports
-import math
-import scipy.stats as stats
+import scipy.stats
+import scipy
 import pandas as pd
 import numpy as np
+
 df = pd.read_csv('data/house_pricing.csv')
 sample = df['GrLivArea']
 
-
-# Write your solution here :
-
-
+def confidence_interval(data, confidence = 0.95):
+    a = 1.0 * np.array(data)
+    n = len(a)
+    m, se = np.mean(a), scipy.stats.sem(a)
+    h = se * scipy.stats.t._ppf((1 + confidence)/ 2., n-1)
+    return m-h, m+h
