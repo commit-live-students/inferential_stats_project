5ba082d81086cdbf418a76e2c2124faa02992b0f
KRSatpute <ksatpute123@gmail.com>
2018-01-06 05:02:41 +0000

    Done

--- q03_t_test/build.py
+++ q03_t_test/build.py
@@ -1,9 +1,23 @@
 # Default imports
+from statsmodels.stats.weightstats import ztest
 import scipy.stats as stats
+import numpy as np
 import pandas as pd
 
 df = pd.read_csv('data/house_pricing.csv')
 
 
 # Enter Code Here
+def t_statistic(df):
+    _, p_value = ztest(
+        x1=df[df['Neighborhood'] == 'OldTown']['GrLivArea'],
+        value=df['GrLivArea'].mean()
+        )
 
+    pvalue = stats.ttest_1samp(
+        a=df[df['Neighborhood'] == 'OldTown']['GrLivArea'],
+        popmean=df['GrLivArea'].mean()
+        )
+
+    test_result = pvalue[1] < p_value
+    return pvalue[1], test_result
