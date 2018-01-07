3004082267e99b5f481b4e85c0751acca1f0fc28
Hazi1990 <hazi1990@gmail.com>
2018-01-07 11:43:06 +0000

    Done

--- q01_cond_prob/build.py
+++ q01_cond_prob/build.py
@@ -8,5 +8,8 @@ df = pd.read_csv('data/house_pricing.csv')
 
 # Enter Code Here
 
-
-
+def cond_prob(df):
+    th = df.shape[0]
+    ho = df[df['Neighborhood']=='OldTown'].shape[0]
+    conditional_prob = (ho/th)*((ho-1)/(th-1))*((ho-2)/(th-2))
+    return conditional_prob
