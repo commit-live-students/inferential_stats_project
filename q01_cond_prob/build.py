97c9b4892a6bd0d698c5cdf77b7e78b519e7e7c8
yuvrajkale87 <yuvraj.kale87@gmail.com>
2018-01-26 14:04:37 +0000

    Done

--- q01_cond_prob/build.py
+++ q01_cond_prob/build.py
@@ -9,4 +9,9 @@ df = pd.read_csv('data/house_pricing.csv')
 # Enter Code Here
 
 
+def cond_prob(df):
+    a = df[df['Neighborhood']=='OldTown']['Neighborhood'].count()
+    b = df.shape[0]
+    conditional_prob = (a/b)*((a-1)/(b-1))*((a-2)/(b-2))
 
+    return conditional_prob
