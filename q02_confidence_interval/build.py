8b5a75ef5ff7e78bd48ce8968a1f6e3c0367be36
AshlinKarkada <ashlin@greyatom.com>
2018-02-09 18:20:05 +0000

    Done

--- q02_confidence_interval/build.py
+++ q02_confidence_interval/build.py
@@ -5,10 +5,7 @@ import scipy.stats as stats
 from greyatomlib import pandas_project as pd
 
 df = pd.read_csv('data/house_pricing.csv')
-
-np.random.seed(10)
 sample = np.random.choice(a=df['GrLivArea'], size=500)
-
 def confidence_interval(sample=sample):
     sample_mean = sample.mean()
     z_critical = stats.norm.ppf(q=0.95)
@@ -16,3 +13,4 @@ def confidence_interval(sample=sample):
     standard_error = z_critical * (stdev / math.sqrt(sample.shape[0]))
     confidence_interval = (sample_mean - standard_error, sample_mean + standard_error)
     return confidence_interval
+
--- q03_t_test/build.py
+++ q03_t_test/build.py
@@ -1,3 +1,4 @@
+
 import scipy.stats as stats
 import pandas as pd
 
@@ -6,3 +7,4 @@ df = pd.read_csv('data/house_pricing.csv')
 def t_statistic(df):
     t_statistic, p_value = stats.ttest_1samp(a=df[df['Neighborhood']=='OldTown']['GrLivArea'], popmean=df['GrLivArea'].mean())
     return p_value, p_value < 0.05
+
