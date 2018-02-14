08303696ce4ca8f177989fc999bf5865d01924d5
tikiabbas <abbas.tiki@gmail.com>
2018-02-14 18:32:26 +0000

    Done

--- q02_confidence_interval/build.py
+++ q02_confidence_interval/build.py
@@ -6,10 +6,13 @@ import pandas as pd
 df = pd.read_csv('data/house_pricing.csv')
 sample = df['GrLivArea']
 
+def confidence_interval(sample):
+    sd = sample.std()
+    se = sd/math.sqrt(len(sample))
+    mean = sample.mean()
+    lcl, ucl =  mean - 1.64485362695*se , mean + 1.64485362695*se
+    return lcl, ucl
 
 # Write your solution here :
-
-
-
-
-
+confidence_interval(sample)
+# Write your solution here :
--- q03_t_test/build.py
+++ q03_t_test/build.py
@@ -1,9 +1,14 @@
+# %load q03_t_test/build.py
 # Default imports
 import scipy.stats as stats
 import pandas as pd
 
 df = pd.read_csv('data/house_pricing.csv')
 
+def t_statistic(df):
 
+    sample = df[df['Neighborhood']=='OldTown']['GrLivArea']
+    t_statistic, p_value = stats.ttest_1samp(a=sample, popmean=df['GrLivArea'].mean())
+    return p_value, p_value < 0.05
 # Enter Code Here
-
+t_statistic(df)
