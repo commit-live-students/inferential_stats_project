36eb6f7d20706e23b866716c9d922d51e6a4470a
gupta1006 <guptagauttam@gmail.com>
2018-02-15 07:48:35 +0000

    Done

--- q03_t_test/build.py
+++ q03_t_test/build.py
@@ -1,3 +1,4 @@
+# %load q03_t_test/build.py
 # Default imports
 import scipy.stats as stats
 import pandas as pd
@@ -5,5 +6,10 @@ import pandas as pd
 df = pd.read_csv('data/house_pricing.csv')
 
 
-# Enter Code Here
+def t_statistic(df):
 
+    a = df[df['Neighborhood'] == 'OldTown']['GrLivArea']
+    popmean = df['GrLivArea'].mean()
+
+    test = stats.ttest_1samp(a,popmean)
+    return test[1], test[1] < 0.05
