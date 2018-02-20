2cfbcde61584f2140e0dc02dda3453311939cf81
brijesh-upadhyay <brijesh_upadhyay@ymail.com>
2018-02-20 17:57:00 +0000

    Done

--- q03_t_test/build.py
+++ q03_t_test/build.py
@@ -9,5 +9,5 @@ def t_statistic(data):
     t_statistic, p_value = stats.ttest_1samp(a= data[data['Neighborhood'] == 'OldTown']['GrLivArea'], popmean= data['GrLivArea'].mean())  # Pop mean
     return float(p_value), p_value < 0.05
 
-print t_statistic(data)
+#print t_statistic(data)
 
