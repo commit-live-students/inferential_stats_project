cfd747f9c913bae1e5cafb7ebbcc216bc68880ee
sohelahmadkhan <sohelahmadkhan000@gmail.com>
2018-02-23 17:56:20 +0000

    Done

--- q01_cond_prob/build.py
+++ q01_cond_prob/build.py
@@ -7,6 +7,13 @@ df = pd.read_csv('data/house_pricing.csv')
 
 
 # Enter Code Here
+def cond_prob(df):
 
+    all_houses = df.shape[0]
+    houses_in_oldtown = df[df['Neighborhood']=='OldTown'].shape[0]
+    conditional_prob =  float((houses_in_oldtown/all_houses)*((houses_in_oldtown-1)/(all_houses-1))*((houses_in_oldtown-2)/(all_houses-2)))
+    #print('Probablity of getting 3 houses in row in Oldtown : {}'.format(conditional_prob))
+   # print type(conditional_prob)
+    return conditional_prob
 
-
+#print cond_prob(df)
