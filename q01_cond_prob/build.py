0d854bad4dcf32660747909a3d79f25231e4c9dc
sonal7930 <sonal.chandra7930@gmail.com>
2018-02-28 10:49:47 +0000

    Done

--- q01_cond_prob/build.py
+++ q01_cond_prob/build.py
@@ -6,7 +6,10 @@ import pandas as pd
 df = pd.read_csv('data/house_pricing.csv')
 
 
-# Enter Code Here
+def cond_prob(df):
+    all_houses = df.shape[0]
+    houses_in_OldTown = df[df['Neighborhood'] == 'OldTown'].shape[0]
+    prob_house_OldTown=houses_in_OldTown/all_houses
+    return((prob_house_OldTown) * ((houses_in_OldTown - 1)/(all_houses - 1))*((houses_in_OldTown - 2)/(all_houses - 2)))
 
-
-
+print cond_prob(df)
