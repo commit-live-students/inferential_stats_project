868e1a45e481e6f16b8a5c953bcd94fd3edb80c0
gitint <suraj@greyatom.com>
2018-02-28 08:29:18 +0000

    Done

--- q01_cond_prob/build.py
+++ q01_cond_prob/build.py
@@ -1,3 +1,4 @@
+# %load q01_cond_prob/build.py
 # So that float division is by default in python 2.7
 from __future__ import division
 
@@ -7,6 +8,9 @@ df = pd.read_csv('data/house_pricing.csv')
 
 
 # Enter Code Here
+
+
+
 
 
 
