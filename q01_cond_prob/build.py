d47cd469c1c1f4de8335a1d6b50835c4aad2807d
kvin15 <vinayakkhamkar@gmail.com>
2018-02-28 17:59:19 +0000

    Done

--- q01_cond_prob/build.py
+++ q01_cond_prob/build.py
@@ -7,6 +7,12 @@ df = pd.read_csv('data/house_pricing.csv')
 
 
 # Enter Code Here
-
-
-
+def cond_prob(inp_df):
+	number_of_houses = inp_df.shape[0]
+	print number_of_houses
+	oldtown_houses = inp_df[inp_df['Neighborhood'] == 'OldTown'].shape[0]
+	prob_oldtown_house = oldtown_houses / number_of_houses
+	prob_oldtown_house_suc3 =   (oldtown_houses / number_of_houses) \
+								* ( (oldtown_houses -1) / (number_of_houses -1)) \
+								* ((oldtown_houses-2) / (number_of_houses -2))
+	return prob_oldtown_house_suc3
