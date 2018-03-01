89bc156b98254623e30f2f4fa5680fab447282d2
HerambD <heramb.dharmadhikari@gmail.com>
2018-03-01 06:18:20 +0000

    Done

--- q04_chi2_test/build.py
+++ q04_chi2_test/build.py
@@ -6,5 +6,16 @@ df = pd.read_csv('data/house_pricing.csv')
 
 
 # Enter Code Here
+import numpy as np
+def chi_square(df):
+    q = pd.qcut(df.SalePrice , q=3, labels=["low", "medium", "High"])
+    freqtab = pd.crosstab(index=df.LandSlope, columns=q)
+    chiSquare_test_statistic, p_value, degree_of_freedom, expected = stats.chi2_contingency(freqtab)
+    significance = (1-.90) / 2 # # 95% significance level. Two-way t-test => .05/ 2 = .025
 
+    if p_value <= significance:
+        reject_null = True
+    else:
+        reject_null = False
 
+    return p_value, np.bool_(reject_null)
