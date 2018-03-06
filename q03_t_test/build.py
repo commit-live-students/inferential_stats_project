2a1e299d97494388c3a60586fdb443726d908a5a
viishnu712 <viishnu712@gmail.com>
2018-03-06 15:23:34 +0000

    Done

--- q03_t_test/build.py
+++ q03_t_test/build.py
@@ -1,9 +1,16 @@
 # Default imports
 import scipy.stats as stats
 import pandas as pd
+import numpy as np
 
 df = pd.read_csv('data/house_pricing.csv')
 
+def t_statistic(df):
+    alpha=0.10
+    stats_1 = stats.ttest_1samp(a= df[df['Neighborhood'] == 'OldTown']['GrLivArea'], popmean= df['GrLivArea'].mean())
+    if stats_1.pvalue > alpha:
+        boolean = np.bool_(False)
+    else:
+        boolean = np.bool_(True)
 
-# Enter Code Here
-
+    return (stats_1.pvalue,(boolean))
