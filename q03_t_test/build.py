144a195b8cf5eb586ee6cd7d7e1871d351106a02
sonal7930 <sonal.chandra7930@gmail.com>
2018-03-07 11:09:33 +0000

    Done

--- q03_t_test/build.py
+++ q03_t_test/build.py
@@ -1,9 +1,16 @@
-# Default imports
+import numpy as np
 import scipy.stats as stats
 import pandas as pd
+#from statsmodels.stats.weightstats import ztest
 
 df = pd.read_csv('data/house_pricing.csv')
 
-
-# Enter Code Here
-
+def t_statistic(df):
+    pop_mean = df['GrLivArea'].mean()
+    N=df[df['Neighborhood'] == 'OldTown']['GrLivArea']
+    N_mean = N.mean()
+    t_test = stats.ttest_1samp(N,pop_mean)
+    p_value=t_test[1]
+    alpha= (p_value*90)/100
+    test_result = np.less(p_value,alpha)
+    return p_value,test_result
