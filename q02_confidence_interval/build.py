e6b1e118973d908e126744e8b7fbf7b428db8079
guptaaman92 <guptaaman92@gmail.com>
2018-03-07 10:42:00 +0000

    Done

--- q02_confidence_interval/build.py
+++ q02_confidence_interval/build.py
@@ -6,8 +6,13 @@ import numpy as np
 df = pd.read_csv('data/house_pricing.csv')
 sample = df['GrLivArea']
 
-
 # Write your solution here :
+def confidence_interval(sample):
+    mean = sample.mean()
+    sd = sample.std()
+    z_value = stats.norm.ppf(q = .95)
+    lower_limit_of_confidence_interval = mean - z_value*(sd/math.sqrt(1460))
+    high = mean + z_value*(sd/math.sqrt(1460))
+    return lower_limit_of_confidence_interval, high
 
-
-
+confidence_interval(sample)
