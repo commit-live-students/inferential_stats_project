4507bccdc684b340df9e5b7919c26188fc58f5bb
kvin15 <vinayakkhamkar@gmail.com>
2018-03-08 12:34:19 +0000

    Done

--- q02_confidence_interval/build.py
+++ q02_confidence_interval/build.py
@@ -8,6 +8,10 @@ sample = df['GrLivArea']
 
 
 # Write your solution here :
-
-
-
+def confidence_interval(sample):
+    confid_intrval_pct = 0.95
+    # Get z score
+    z_score = stats.norm.ppf(q=confid_intrval_pct)
+    mean_val = sample.mean()
+    margin_of_error = z_score * sample.std() / math.sqrt(sample.size)
+    return (mean_val - margin_of_error,mean_val + margin_of_error)
