7ac20614d4d6841250dde8ea3527f0d3176e9dbb
kvin15 <vinayakkhamkar@gmail.com>
2018-03-09 14:18:23 +0000

    Done

--- q03_t_test/build.py
+++ q03_t_test/build.py
@@ -6,4 +6,18 @@ df = pd.read_csv('data/house_pricing.csv')
 
 
 # Enter Code Here
+def t_statistic(data):
+    # considering two tailed t-test and confidence interval given is 90%
+    # as we are trying with null hypothesis of
+    # "The mean of Living area of houses in OldTown is the same as the population mean"
+    exp_p_value = (1 - 0.9) / 2
+    gr_livng_area = df['GrLivArea']
+    # Get the mean of the population
+    pop_mean = gr_livng_area.mean()
+    # Get OldTown's GrLivArea data as Sample
+    gr_livng_area_sample = df[df['Neighborhood']=='OldTown'].GrLivArea
+    p_value = stats.ttest_1samp(a=gr_livng_area_sample,popmean=pop_mean).pvalue
+    # print "Expected Value {} and Calculated Value {}".format(exp_p_value,p_value)
+    t_test_result = p_value < exp_p_value
 
+    return (p_value, t_test_result)
