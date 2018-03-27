e71309123f7612deb90b60579d49764c45fba28d
sacvijai <sacvijai@gmail.com>
2018-03-27 18:05:29 +0000

    Done

--- q04_chi2_test/build.py
+++ q04_chi2_test/build.py
@@ -1,10 +1,19 @@
 # Default imports
 import scipy.stats as stats
 import pandas as pd
-
+import numpy as np
 df = pd.read_csv('data/house_pricing.csv')
-
+bol = np.bool_(dtype=bool)
 
 # Enter Code Here
-
-
+def chi_square(data):
+    crit = stats.chi2.ppf(q = 0.95, # Find the critical value for 95% confidence*
+                      df = 2)
+    price= pd.qcut(data['SalePrice'], 3, labels=["High","medium","Low"])
+    freqtab = pd.crosstab(data['LandSlope'],price)
+    chi2,pval,dof,expected = stats.chi2_contingency(freqtab)
+    if chi2 > crit:
+        bol = np.bool_(True)
+    else:
+        bol = np.bool_(False)
+    return pval,bol
