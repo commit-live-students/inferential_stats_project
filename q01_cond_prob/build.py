6672eff17d25442e626d3cce91d9ee6cfbbc54a8
sejal-varu <sejalvaru@gmail.com>
2018-04-07 04:44:16 +0000

    Done

--- q01_cond_prob/build.py
+++ q01_cond_prob/build.py
@@ -7,6 +7,10 @@ df = pd.read_csv('data/house_pricing.csv')
 
 
 # Enter Code Here
+def cond_prob(df):
+    total_houses = float(len(df.index))
+    oldtown_houses = float(len(df[df["Neighborhood"] == "OldTown"]))
+    proba = (oldtown_houses / total_houses) * ((oldtown_houses-1)/(total_houses-1)) * ((oldtown_houses-2)/(total_houses-2))
+    return proba
 
-
-
+cond_prob(df)
