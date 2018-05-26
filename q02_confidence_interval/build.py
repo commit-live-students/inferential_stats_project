5ae974d313629238b5586ed8cf46d6db74fb643a
bharadwajk9 <utkb.k9@gmail.com>
2018-05-26 15:41:09 +0000

    Done

--- q02_confidence_interval/build.py
+++ q02_confidence_interval/build.py
@@ -1,3 +1,4 @@
+# %load q02_confidence_interval/build.py
 # Default imports
 import math
 import scipy.stats as stats
@@ -8,6 +9,18 @@ sample = df['GrLivArea']
 
 
 # Write your solution here :
+def confidence_interval(sample):
+    sample_mean = sample.mean()
+    sample_size = sample.count()
+    sample_stdev = sample.std()
+    z_critical = stats.norm.ppf(q=0.95)
+    margin_of_error = z_critical * (sample_stdev/math.sqrt(sample_size))
+    confidence_interval = (sample_mean-margin_of_error,
+                           sample_mean+margin_of_error)
+    return confidence_interval
+
+confidence_interval(sample)
+
 
 
 
