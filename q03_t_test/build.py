4db6c2506c5198fa8d1b0f63063176127858a399
kravigupta <kravi.gupta@gmail.com>
2018-06-05 14:46:13 +0000

    Done

--- q03_t_test/build.py
+++ q03_t_test/build.py
@@ -1,3 +1,4 @@
+# %load q03_t_test/build.py
 # Default imports
 import scipy.stats as stats
 import pandas as pd
@@ -6,4 +7,18 @@ df = pd.read_csv('data/house_pricing.csv')
 
 
 # Enter Code Here
+def t_statistic(df):
+    # H0 => mean_liv_area_oldTown = mean_liv_area_pop
+    # H1 => mean_liv_area_oldTown != mean_liv_area_pop
+    # significance level 90%
+    old_town_data = df[df['Neighborhood'] == 'OldTown']['GrLivArea']
+    pop_mean = df['GrLivArea'].mean()
+    results = stats.ttest_1samp(a=old_town_data,popmean=pop_mean)
+    pvalue = results.pvalue
+    test_result = results.statistic
+    reject_h0 = pvalue < 0.1
+    return pvalue, reject_h0
+
+t_statistic(df)
+
 
