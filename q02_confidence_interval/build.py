3e572b76b6d6878691783c67727086af52c0d5e7
siddesh001 <siddesh.001@gmail.com>
2018-06-06 16:01:27 +0000

    Done

--- q02_confidence_interval/build.py
+++ q02_confidence_interval/build.py
@@ -1,3 +1,4 @@
+# %load q02_confidence_interval/build.py
 # Default imports
 import math
 import scipy.stats as stats
@@ -8,6 +9,20 @@ sample = df['GrLivArea']
 
 
 # Write your solution here :
+
+def confidence_interval(sample):
+    standard_dev=(sample.std())
+    sample_size=(sample.count())
+    
+    standard_error=(standard_dev)/(math.sqrt(sample_size))
+    
+    z = stats.norm.ppf(q = 0.95)
+    
+    estimate=z*standard_error
+    
+    ci_p=np.mean(sample)+estimate
+    ci_n=np.mean(sample)-estimate
+    return (ci_n,ci_p)
 
 
 
