374d01f220da61f4f51e1daa6256742e76e9265f
naitik1995 <naitikgada1995@gmail.com>
2018-06-09 17:04:36 +0000

    Done

--- q03_t_test/build.py
+++ q03_t_test/build.py
@@ -1,9 +1,18 @@
+# %load q03_t_test/build.py
 # Default imports
 import scipy.stats as stats
 import pandas as pd
 
 df = pd.read_csv('data/house_pricing.csv')
-
-
 # Enter Code Here
+def t_statistic(df):
+    old_town = df[df['Neighborhood']  == 'OldTown']['GrLivArea']
+    population = df['GrLivArea']
+    population_mean = population.mean()
+    tstats = stats.ttest_1samp(old_town,population_mean)
+    pvalue = tstats.pvalue
+    reject_h = pvalue < 0.1
+    return pvalue,reject_h
+t_statistic(df)
+
 
