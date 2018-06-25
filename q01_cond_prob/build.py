dc9db7f8a0f2e27a6a913a344c5516589c65c664
ankutty <akutty@deloitte.com>
2018-06-25 13:48:26 +0000

    Done

--- q01_cond_prob/build.py
+++ q01_cond_prob/build.py
@@ -1,3 +1,4 @@
+# %load q01_cond_prob/build.py
 # So that float division is by default in python 2.7
 from __future__ import division
 
@@ -5,8 +6,19 @@ import pandas as pd
 
 df = pd.read_csv('data/house_pricing.csv')
 
-
 # Enter Code Here
+def cond_prob(dataframe):
+    all_houses = dataframe.shape[0]
+    old_town_houses = dataframe[dataframe['Neighborhood'] == 'OldTown']
+    all_old_town_houses = old_town_houses.shape[0]
+    
+    prob_1 = (all_old_town_houses)/(all_houses)
+    prob_2 = (all_old_town_houses - 1)/(all_houses - 1)
+    prob_3 = (all_old_town_houses - 2)/(all_houses - 2)
+    
+    probability = prob_1 * prob_2 * prob_3
+    return probability
 
-
+output = cond_prob(df)
+print(output)
 
