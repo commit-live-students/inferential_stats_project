64e351f100236983485899258d115c6d647d8ea0
Vagus30 <vishwas.tejaswi@gmail.com>
2018-07-11 10:23:37 +0000

    Done

--- q02_confidence_interval/build.py
+++ q02_confidence_interval/build.py
@@ -1,3 +1,4 @@
+# %load q02_confidence_interval/build.py
 # Default imports
 import math
 import scipy.stats as stats
@@ -5,9 +6,19 @@ import pandas as pd
 import numpy as np
 df = pd.read_csv('data/house_pricing.csv')
 sample = df['GrLivArea']
-
+n = 1000
 
 # Write your solution here :
+def confidence_interval(sample=sample):
+    sample_mean = sample.mean()
+    z_critical = stats.norm.ppf(q=0.95)
+    stdev = sample.std()
+    standard_error = z_critical*(stdev/math.sqrt(sample.shape[0]))
+    confidence_interval = (sample_mean-standard_error,sample_mean+standard_error)
+    return confidence_interval
+    
+    
+
 
 
 
