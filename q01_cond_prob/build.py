eb206aaeca30e53ba9838707705d9846fc13852e
TheSarang <narkhedesarang@gmail.com>
2018-07-23 11:53:08 +0000

    Done

--- q01_cond_prob/build.py
+++ q01_cond_prob/build.py
@@ -1,3 +1,4 @@
+# %load q01_cond_prob/build.py
 # So that float division is by default in python 2.7
 from __future__ import division
 
@@ -6,7 +7,13 @@ import pandas as pd
 df = pd.read_csv('data/house_pricing.csv')
 
 
-# Enter Code Here
+def cond_prob(df):
+    n = df[df['Neighborhood'] == 'OldTown'].shape[0]
+    d = df['Neighborhood'].shape[0]
+    return ((n/d)*((n-1)/(d-1))*((n-2)/(d-2)))
+    
+cond_prob(df)
+
 
 
 
