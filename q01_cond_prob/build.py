8cde9a927bec373f21aff17bb2ec3ba5761ed526
aditya207 <adityam207@gmail.com>
2018-07-23 09:43:09 +0000

    Done

--- q01_cond_prob/build.py
+++ q01_cond_prob/build.py
@@ -1,3 +1,4 @@
+# %load q01_cond_prob/build.py
 # So that float division is by default in python 2.7
 from __future__ import division
 
@@ -7,6 +8,17 @@ df = pd.read_csv('data/house_pricing.csv')
 
 
 # Enter Code Here
+def cond_prob(df):
+    oldTownLen = len(df[df['Neighborhood'] == 'OldTown'])
+    totalLen = len(df['Neighborhood'])
+    print(totalLen)
+    conditional_prob = (oldTownLen / totalLen) * ((oldTownLen - 1) / totalLen) * ((oldTownLen-2) / totalLen)
+    return conditional_prob
+cond_prob(df)
+
+
+
+
 
 
 
