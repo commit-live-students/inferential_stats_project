61c924e95ecee98c97987ce21eb9b4e4faab4f05
Sumeshks29 <sumeshks29@gmail.com>
2018-08-03 13:35:41 +0000

    Done

--- q03_t_test/build.py
+++ q03_t_test/build.py
@@ -1,9 +1,16 @@
+# %load q03_t_test/build.py
 # Default imports
 import scipy.stats as stats
 import pandas as pd
-
+import numpy as np
 df = pd.read_csv('data/house_pricing.csv')
 
 
 # Enter Code Here
+def t_statistic(data):
+    
+    zstats,pvalue=stats.ttest_1samp(a= data[data['Neighborhood'] == 'OldTown']['GrLivArea'],popmean= data['GrLivArea'].mean()) 
+    zstats=np.bool_(True) if(zstats>0) else np.bool_(False)
+    return pvalue,zstats
+
 
