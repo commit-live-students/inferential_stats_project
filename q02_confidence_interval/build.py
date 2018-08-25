400d3588c5c69996f796f04b268d6dbd2fc8fd19
prasannajayashankar <prasanna.jay@gmail.com>
2018-08-25 06:44:15 +0000

    Done

--- q02_confidence_interval/build.py
+++ q02_confidence_interval/build.py
@@ -1,3 +1,4 @@
+#*SE %load q02_confidence_interval/build.py
 # Default imports
 import math
 import scipy.stats as stats
@@ -9,5 +10,14 @@ sample = df['GrLivArea']
 
 # Write your solution here :
 
+def confidence_interval(sample):
+    SE = sample.std()/(len(sample)**.5)
+    ll = sample.mean()- 1.644853626951477*SE
+    ul = sample.mean()+ 1.644853626951477*SE
+    return ll,ul
+
+
+SE = sample.std()/(len(sample)**.5)
+(1538.0844661828817-sample.mean())/SE
 
 
