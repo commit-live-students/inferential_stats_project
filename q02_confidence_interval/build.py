abb7af680c5a12e7ffadc14747fe44833e7c5270
sonik8494 <sourabh.niks1994@gmail.com>
2018-09-06 07:42:30 +0000

    Done

--- q02_confidence_interval/build.py
+++ q02_confidence_interval/build.py
@@ -1,3 +1,4 @@
+# %load q02_confidence_interval/build.py
 # Default imports
 import math
 import scipy.stats as stats
@@ -5,6 +6,29 @@ import pandas as pd
 import numpy as np
 df = pd.read_csv('data/house_pricing.csv')
 sample = df['GrLivArea']
+
+def confidence_interval(sample):
+    
+    n = sample.shape[0]
+
+    std = df['GrLivArea'].std()
+
+    mean = np.mean(sample)
+
+    error = std/(math.sqrt(n))
+
+    z_value = stats.norm.ppf(q = 0.95)
+
+    estimate = z_value* error
+
+    low = mean - estimate
+    high = mean + estimate
+
+    return low,high
+    
+
+confidence_interval(sample)
+
 
 
 # Write your solution here :
