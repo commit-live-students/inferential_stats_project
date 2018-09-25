79c9701ac9ff7b7c16efc0db86aba908148eb16e
aishkl <aishkl123@gmail.com>
2018-09-25 08:36:48 +0000

    Done

--- q03_t_test/build.py
+++ q03_t_test/build.py
@@ -1,9 +1,25 @@
+# %load q03_t_test/build.py
 # Default imports
 import scipy.stats as stats
 import pandas as pd
-
+import numpy as np
+from statsmodels.stats.weightstats import ztest
 df = pd.read_csv('data/house_pricing.csv')
 
 
 # Enter Code Here
+def t_statistic(df):
+    z_statistics,p_value=stats.ttest_1samp(df[df['Neighborhood']=='OldTown']['GrLivArea']
+                              ,df['GrLivArea'].mean())
+    z_critical = stats.norm.ppf(q = 0.90)
+    if(p_value < z_critical):
+        return p_value,np.False_
+    else:
+        return p_value,np.True_
+
+
+t_statistic(df)
+
+
+
 
--- q04_chi2_test/build.py
+++ q04_chi2_test/build.py
@@ -1,3 +1,4 @@
+# %load q04_chi2_test/build.py
 # Default imports
 import scipy.stats as stats
 import pandas as pd
@@ -6,5 +7,20 @@ df = pd.read_csv('data/house_pricing.csv')
 
 
 # Enter Code Here
+def chi_square(df):
+    price = pd.qcut(df['SalePrice'], 3, labels = ['High', 'Medium', 'Low'])
+    LandSlope = df['LandSlope']
+    freqtab = pd.crosstab(price,LandSlope)
+    print(freqtab)
+    chi2,pval,dof,expected=stats.chi2_contingency(freqtab)
+    z_value=stats.chi2.ppf(q=0.95,df=2)
+    print(z_value)
+    if(pval>z_value):
+        return True,pval
+    else:
+        return False,pval
+chi_square(df)
+
+
 
 
