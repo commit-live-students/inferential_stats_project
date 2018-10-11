83ad0c3f0b11a6b8616e756bec7dc75df7493b92
sunilhariharan <sunilhariharan@gmail.com>
2018-10-11 08:19:11 +0000

    Done

--- q03_t_test/build.py
+++ q03_t_test/build.py
@@ -1,9 +1,22 @@
+# %load q03_t_test/build.py
 # Default imports
 import scipy.stats as stats
 import pandas as pd
 
 df = pd.read_csv('data/house_pricing.csv')
 
+def t_statistic(df):
+    alpha=0.01
+    df1=df.groupby('Neighborhood').get_group('OldTown')
+    u=df['GrLivArea'].mean()
+    x_bar=df1['GrLivArea'].mean()
+    sigma=df['GrLivArea'].std()
+    n=len(df1['Neighborhood'].values)
+    z=(x_bar-u)/(sigma/(n**0.5))
+    p_value=stats.norm.cdf(z)
+    if p_value<alpha:
+        return p_value,False
+    else:
+        return p_value,True
 
-# Enter Code Here
 
--- q04_chi2_test/build.py
+++ q04_chi2_test/build.py
@@ -1,10 +1,15 @@
+# %load q04_chi2_test/build.py
 # Default imports
 import scipy.stats as stats
 import pandas as pd
-
+import numpy as np
 df = pd.read_csv('data/house_pricing.csv')
 
-
-# Enter Code Here
+def chi_square(df):
+    price = pd.qcut(df['SalePrice'], 3, labels = ['High', 'Medium', 'Low'])
+    freqtab = pd.crosstab(df.LandSlope,price)
+    chi2,p_value,dof,expected = stats.chi2_contingency(freqtab)
+    test_result=np.all(expected>5)
+    return p_value,test_result
 
 
