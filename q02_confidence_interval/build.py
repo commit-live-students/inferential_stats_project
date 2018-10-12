b52a578f27fc56e46ac58622bb6d82aee7c93ede
prodigy803 <pushkaraj803@gmail.com>
2018-10-12 13:16:04 +0000

    Done

--- q02_confidence_interval/build.py
+++ q02_confidence_interval/build.py
@@ -1,3 +1,4 @@
+# %load q02_confidence_interval/build.py
 # Default imports
 import math
 import scipy.stats as stats
@@ -8,6 +9,23 @@ sample = df['GrLivArea']
 
 
 # Write your solution here :
+def confidence_interval(df):
+    mean = np.mean(sample)
+    stand_error = stats.sem(sample)
+    print(stand_error)
+    z_value = 1.96
+    low_val = mean - (z_value * stand_error)
+    up_val = mean + (z_value*stand_error)
+    return low_val,up_val
+confidence_interval(df)
+a = sample
+n = len(a)
+m, se = np.mean(a), stats.sem(a)
+print(se)
+h = se * stats.t.ppf((1 + 0.90) / 2., n)
+print(m-h, m+h)
+import numpy as np, scipy.stats as st
 
+st.t.interval(0.9, len(a)-1, loc=np.mean(a), scale=st.sem(a))
 
 
--- q03_t_test/build.py
+++ q03_t_test/build.py
@@ -1,9 +1,26 @@
+# %load q03_t_test/build.py
 # Default imports
 import scipy.stats as stats
 import pandas as pd
-
+import numpy as np
 df = pd.read_csv('data/house_pricing.csv')
 
 
 # Enter Code Here
+def t_statistic(df):
+    
+    houses_OLDTOWN = df[df['Neighborhood']=='OldTown'].GrLivArea
+    
+    p_value = (stats.ttest_1samp(a =houses_OLDTOWN , popmean=df.GrLivArea.mean())).pvalue
+    
+    x= (stats.ttest_1samp(a=df.GrLivArea,popmean=df.GrLivArea.mean())).statistic
+    
+    t = stats.t.cdf(x,df=49)
+    
+    return p_value, np.bool_(t > p_value)
+#0.51158698884870502
+#t_statistic(df)
+
+
+
 
