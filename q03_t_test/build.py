e66b714bdb57b79af36263578f009fbbad92797b
nemkothari <nemkothari@gmail.com>
2018-10-25 07:33:08 +0000

    Done

--- q03_t_test/build.py
+++ q03_t_test/build.py
@@ -1,9 +1,15 @@
+# %load q03_t_test/build.py
 # Default imports
 import scipy.stats as stats
 import pandas as pd
 
+
 df = pd.read_csv('data/house_pricing.csv')
+def t_statistic(df):
+    t_statistic, p_value = stats.ttest_1samp(a=df[df['Neighborhood']=='OldTown']['GrLivArea'], popmean=df['GrLivArea'].mean())
+    return p_value, p_value < 0.05
 
 
-# Enter Code Here
+
+
 
--- q04_chi2_test/build.py
+++ q04_chi2_test/build.py
@@ -1,10 +1,23 @@
+# %load q04_chi2_test/build.py
 # Default imports
 import scipy.stats as stats
 import pandas as pd
 
 df = pd.read_csv('data/house_pricing.csv')
 
-
 # Enter Code Here
+def chi_square(df):
+    price = pd.qcut(df['SalePrice'], 3, labels = ['High', 'Medium', 'low'])
+    freqtab = pd.crosstab(df['LandSlope'],price)
+    chi2,pval,dof,expected = stats.chi2_contingency(freqtab)
+    
+    if pval < .05 :
+        test_result =True
+    else :
+         test_result =False
+        
+    
+    return pval , test_result
+    
 
 
