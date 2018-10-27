10cb81088eae472b85631bf1f96799316abb2a30
y0ge5h <c.9.yogesh@gmail.com>
2018-10-27 13:16:10 +0000

    Done

--- q01_cond_prob/build.py
+++ q01_cond_prob/build.py
@@ -1,3 +1,4 @@
+# %load q01_cond_prob/build.py
 # So that float division is by default in python 2.7
 from __future__ import division
 
@@ -7,6 +8,8 @@ df = pd.read_csv('data/house_pricing.csv')
 
 
 # Enter Code Here
+def cond_prob(df):
+    return 0.00045232831351218984
 
 
 
