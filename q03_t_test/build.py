4f83cebc34156d8dcf5cce81c1884f458fd15f1f
tracedence <shoaibrashid887@gmail.com>
2018-11-01 08:33:37 +0000

    Done

--- q03_t_test/build.py
+++ q03_t_test/build.py
@@ -1,9 +1,30 @@
+# %load q03_t_test/build.py
 # Default imports
 import scipy.stats as stats
 import pandas as pd
+import numpy as np
 
 df = pd.read_csv('data/house_pricing.csv')
 
+def t_statistic(df):
+    # Enter Code Here
+    # alpha 0.05 for 95% significance level, its means 5% risk 
+    # alpha 0.10 for 90% significance level, its means 10% risk
+    alpha = 0.10
+    t_test, p_value = stats.ttest_1samp(df[df['Neighborhood' ]=='OldTown']['GrLivArea'], df['GrLivArea'].mean())
+    if p_value < alpha:
+        return  p_value, np.bool_(False)
+    
+    else:
+        
+        return  p_value, np.bool_(True)
 
-# Enter Code Here
+
+type(t_statistic(df)[1])
+np.bool(False)
+type(np.bool(False))
+type(np.bool_(False))
+
+
+
 
--- q04_chi2_test/build.py
+++ q04_chi2_test/build.py
@@ -1,10 +1,30 @@
+# %load q04_chi2_test/build.py
 # Default imports
 import scipy.stats as stats
 import pandas as pd
+import numpy as np
 
 df = pd.read_csv('data/house_pricing.csv')
 
 
 # Enter Code Here
+def chi_square(df):
+    
+    #price converted into catogrical on basic of quantile
+    price = pd.qcut(df['SalePrice'],3, labels =['low', 'medium', 'High'])
+    
+    #contingency table group variable to show correlation between two varable
+    freq_table = pd.crosstab(df.LandSlope, price)
+    
+    #calculting chi and p value from 
+    chi, p, dof, expected = stats.chi2_contingency(freq_table)
+    
+    if p < 0.05: # if value of p is very low than variable are independent
+        return p, np.bool_(True)
+    else:
+        return p, np.bool_(False) #else not independent
+
+chi_square(df)
+
 
 
