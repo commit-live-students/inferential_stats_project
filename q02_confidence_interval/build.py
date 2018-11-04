df6b39ff370c4f853c1d8bf8a9203e5f19ac7b1e
Lavi1920 <lavitha1920@gmail.com>
2018-11-04 14:26:50 +0000

    Done

--- q02_confidence_interval/build.py
+++ q02_confidence_interval/build.py
@@ -1,3 +1,4 @@
+# %load q02_confidence_interval/build.py
 # Default imports
 import math
 import scipy.stats as stats
@@ -5,9 +6,22 @@ import pandas as pd
 import numpy as np
 df = pd.read_csv('data/house_pricing.csv')
 sample = df['GrLivArea']
-
+#print(sample)
 
 # Write your solution here :
+def confidence_interval(df):
+    mean = np.mean(sample)
+    print('mean=', mean)
+    stand_error = stats.sem(sample)
+    print('std_err=',stand_error)
+    z_value = stats.norm.ppf(0.95)
+    print('z value=', z_value)
+    low_val = mean - (z_value * stand_error)
+    up_val = mean + (z_value*stand_error)
+    return low_val,up_val
+    
+confidence_interval(df)
+
 
 
 
