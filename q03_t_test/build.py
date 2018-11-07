4a49c217c5d98f360cfbce7b75c98e1fc462b46f
preetiail <ailpreeti@ymail.com>
2018-11-07 18:36:14 +0000

    Done

--- q03_t_test/build.py
+++ q03_t_test/build.py
@@ -1,3 +1,4 @@
+# %load q03_t_test/build.py
 # Default imports
 import scipy.stats as stats
 import pandas as pd
@@ -6,4 +7,9 @@ df = pd.read_csv('data/house_pricing.csv')
 
 
 # Enter Code Here
+def t_statistic(df):
+    zstat,pval=stats.ttest_1samp(df[df['Neighborhood']=='OldTown']['GrLivArea'],df['GrLivArea'].mean())
+    return(pval,(pval<0.1))
+
+t_statistic(df)
 
