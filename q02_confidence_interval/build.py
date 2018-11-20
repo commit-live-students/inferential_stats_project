18fee0b3e8eaf0cd78c708102fbb4be706b54369
RohanUnni <rohanunni98@yahoo.com>
2018-11-20 11:44:56 +0000

    Done

--- q02_confidence_interval/build.py
+++ q02_confidence_interval/build.py
@@ -1,3 +1,4 @@
+# %load q02_confidence_interval/build.py
 # Default imports
 import math
 import scipy.stats as stats
@@ -9,5 +10,12 @@ sample = df['GrLivArea']
 
 # Write your solution here :
 
-
+def confidence_interval(sample):
+    sample_mean=sample.mean()
+    z_critical = stats.norm.ppf(q = 0.95)  # Get the z-critical value*
+    sam_stdev = df['GrLivArea'].std()
+    margin=z_critical*(sam_stdev/math.sqrt(df.GrLivArea.shape[0]))
+    conf=(sample_mean - margin, sample_mean + margin)
+    return conf
+confidence_interval(sample)
 
