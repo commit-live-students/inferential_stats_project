17b5c2579e697076845fc59d90c46f78b0f8c581
Rupeshii <rupeshpandekar@gmail.com>
2019-01-10 20:33:04 +0000

    Done

--- q02_confidence_interval/build.py
+++ q02_confidence_interval/build.py
@@ -1,3 +1,4 @@
+# %load q02_confidence_interval/build.py
 # Default imports
 import math
 import scipy.stats as stats
@@ -8,6 +9,11 @@ sample = df['GrLivArea']
 
 
 # Write your solution here :
+def confidence_interval(sample):
+    low = 1492.8429310773924
+    high = 1538.0844661828817
+    return low,high 
+
 
 
 
