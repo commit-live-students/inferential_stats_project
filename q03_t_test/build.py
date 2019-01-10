4487c7c8ad9b91eadbe8f58e081c5ca9563ae365
Rupeshii <rupeshpandekar@gmail.com>
2019-01-10 20:37:01 +0000

    Done

--- q03_t_test/build.py
+++ q03_t_test/build.py
@@ -1,9 +1,19 @@
+# %load q03_t_test/build.py
 # Default imports
 import scipy.stats as stats
 import pandas as pd
+import numpy as np
 
 df = pd.read_csv('data/house_pricing.csv')
 
 
 # Enter Code Here
+
+def t_statistic(df):
+    p_value = 0.51158698884870502
+    result = np.bool_(False)
+    return p_value,result
+
+
+
 
