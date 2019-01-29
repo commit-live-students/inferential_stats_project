fc182093722f3ce15ef1946f980462b5b5ca6f8f
sagark93 <sagarkulkarni093@gmail.com>
2019-01-29 07:16:30 +0000

    Done

--- q03_t_test/build.py
+++ q03_t_test/build.py
@@ -1,9 +1,23 @@
+# %load q03_t_test/build.py
 # Default imports
 import scipy.stats as stats
 import pandas as pd
+from statsmodels.stats.weightstats import ztest
 
 df = pd.read_csv('data/house_pricing.csv')
 
 
 # Enter Code Here
+
+
+#def t_statistic(df=df):
+ #   mean_liv_area = df['GrLivArea'].mean()
+  #  for x in df['GrLivArea']:
+        z_statistic, p_value = ztest( (df['GrLivArea'] > df['GrLivArea'].mean()) | (df['GrLivArea'] < df['GrLivArea'].mean()),
+            value=df['GrLivArea'].mean())
+        #print(z_statistic,p_value)
+    return p_value,x
+t_statistic(df)
+#df['GrLivArea'].mean()
+
 
--- q04_chi2_test/build.py
+++ q04_chi2_test/build.py
@@ -1,10 +1,34 @@
+# %load q04_chi2_test/build.py
 # Default imports
 import scipy.stats as stats
 import pandas as pd
+import numpy as np
 
 df = pd.read_csv('data/house_pricing.csv')
 
 
 # Enter Code Here
+
+
+#ser, bins = pd.qcut(df['A'], 20, retbins=True, labels=False)
+
+SalePrice_divided, bins = pd.qcut(df['SalePrice'],3,retbins = True, 
+                                  labels = False)
+#SalePrice_divided
+#df.head()
+def chi_square(df):
+    SalePrice_divided, bins = pd.qcut(df['SalePrice'],3,retbins = True, 
+                                  labels = False)
+    f_obs = pd.crosstab(df['LandSlope'],SalePrice_divided)
+    chi2, p, dof, expected = stats.chi2_contingency(f_obs)
+
+    return p, chi2 < 0.5
+chi_square(df)
+#chi_square()
+SalePrice_divided, bins = pd.qcut(df['SalePrice'],3,retbins = True, 
+                                  labels = False)
+SalePrice_divided
+chi_square(df)
+#stats.chi2_contingency(SalePrice_divided)
 
 
